6038c049f778f1ce0248520fc9497189135c6187
Hannes Moser <hannes.moser@mailbox.org>
2018-05-02 21:53:05 +0200

    Make links open a new tab

--- autohp/autohp.sh
+++ autohp/autohp.sh
@@ -198,8 +198,8 @@ activated]</noscript>
         <p>
         <br>
         <span class="texto">
-        This page is no longer maintained. See also <a href="http://www.informatik.uni-trier.de/~ley/pers/hd/m/Moser:Hannes.html">my publications</a> at <a href="http://www.informatik.uni-trier.de/~ley/db/welcome.html">DBLP</a>,
-        and <a href="https://academic.microsoft.com/#/search?iq=@Hannes%20Moser@&q=Hannes%20Moser&filters=&from=0&sort=0">my publications and citations</a> at <a href="http://academic.research.microsoft.com/">Microsoft Academic Search</a>.
+        This page is no longer maintained. See also <a target="_blank" href="http://www.informatik.uni-trier.de/~ley/pers/hd/m/Moser:Hannes.html">my publications</a> at <a target="_blank" href="http://www.informatik.uni-trier.de/~ley/db/welcome.html">DBLP</a>,
+        and <a target="_blank" href="https://academic.microsoft.com/#/search?iq=@Hannes%20Moser@&q=Hannes%20Moser&filters=&from=0&sort=0">my publications and citations</a> at <a target="_blank" href="http://academic.research.microsoft.com/">Microsoft Academic Search</a>.
         </span>
         </p>
 
